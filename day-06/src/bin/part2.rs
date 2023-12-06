7d3cdd601e73c6f489f9f78e35eb69b621c06f27
Nigel <nigel.nderi@gmail.com>
2023-12-06 03:18:46 -0800

    small refactor for day-06 part 2

--- day-06/src/bin/part2.rs
+++ day-06/src/bin/part2.rs
@@ -6,7 +6,7 @@ fn main() {
     dbg!(output);
 }
 
-fn parse_tagged_num_list(t: &str, line: &str) -> i64{
+fn parse_tagged_num(t: &str, line: &str) -> i64{
     let (nums, _) = terminated(tag(t), multispace1::<&str, error::Error<_>>)(line).expect("line should start with a tag");
 
         separated_list1(space1::<&str, error::Error<_>>, digit0)(nums).expect("should be parseable list of nums").1
@@ -14,19 +14,19 @@ fn parse_tagged_num_list(t: &str, line: &str) -> i64{
             .parse::<i64>().expect("should be a number")
 }
 
-fn calculate_hold_time((t,d): (&i64, &i64)) -> (u64, u64) {
+fn calculate_hold_time(t: i64 , d: i64) -> (u64, u64) {
     let tsquared = t.pow(2) as f64;
     let fourac = (4 * d) as f64; 
     let lower = ((-t as f64 + (tsquared - fourac).sqrt())/-2_f64).floor() as u64 + 1;
-    let upper = ((*t as f64 + (tsquared - fourac).sqrt())/2_f64).ceil() as u64;
+    let upper = ((t as f64 + (tsquared - fourac).sqrt())/2_f64).ceil() as u64;
     (lower, upper)
 }
 
 fn part1(input: &str) -> u64 {
     let mut lines = input.lines();
-    let time = parse_tagged_num_list("Time:", lines.next().expect("should have a time line").trim());
-    let distance = parse_tagged_num_list("Distance: ", lines.next().expect("should have a destination line").trim());
-    let (low, upp) = calculate_hold_time((&time, &distance));
+    let time = parse_tagged_num("Time:", lines.next().expect("should have a time line").trim());
+    let distance = parse_tagged_num("Distance: ", lines.next().expect("should have a destination line").trim());
+    let (low, upp) = calculate_hold_time(time, distance);
     upp - low
 }
 
